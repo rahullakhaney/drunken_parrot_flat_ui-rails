b97431860a947aa59481cfcf9dc186ef7e427186
Tim <timshingyu@gmail.com>
2015-01-20 13:18:05 +0800

    Changed engine initializer

--- lib/drunken_parrot_flat_ui/rails.rb
+++ lib/drunken_parrot_flat_ui/rails.rb
@@ -4,7 +4,7 @@ module DrunkenParrotFlatUi
   module Rails
     class Engine < ::Rails::Engine
         initializer 'drunken_parrot_flat_ui-rails.setup',
-        	:after => 'less-rails.before.load_config_initializers',
+        	:after => 'less-rails.after.load_config_initializers',
         	:group => :all do |app|
             if defined?(Less)
               	app.config.less.paths << File.join(config.root, 'app', 'assets', 'less')
