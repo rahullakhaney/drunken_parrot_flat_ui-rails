3d645cdb3297d1a05c68785197e34f56341933e4
Tim <timshingyu@gmail.com>
2015-01-20 15:56:40 +0800

    Fixing demo generator

--- lib/generators/drunken/demo/templates/drunkenparrotflatui_demo_controller.rb
+++ lib/generators/drunken/demo/templates/drunkenparrotflatui_demo_controller.rb
@@ -1,27 +1,13 @@
 class DrunkenparrotflatuiDemoController < ApplicationController
-  layout :layout_for_page
-
   def index
+    render layout: "index"
   end
 
   def documentation
+    render layout: "documentation"
   end
 
   def start_here
-  end
-
-  private
-
-  def layout_for_page
-    case params[:id]
-    when 'index'
-      'index'
-    when 'documentation'
-      'documentation'
-    when 'start_here'
-      'start_here'      
-    else
-      'application'
-    end
-  end  
+    render layout: "start_here"
+  end 
 end
